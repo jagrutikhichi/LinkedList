7a3241b6d669b8812db11c8def92857750506a40
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-07-01 14:12:45 +0530

    Added: Created Linked List

--- src/com/bridgelabs/LinkedList.java
+++ src/com/bridgelabs/LinkedList.java
@@ -1,5 +1,29 @@
 package com.bridgelabs;
 
 public class LinkedList {
-	
+	MyNode head;
+	public void append(int data) {
+		
+		MyNode newNode = new MyNode();
+		newNode.data = data;
+		newNode.next = null;
+		if(head == null) {
+			this.head = newNode;
+		}
+		else {
+			MyNode tempNode = head;
+			while(tempNode.next != null) {
+				tempNode = tempNode.next;
+			}
+			tempNode.next = newNode;
+		}
+	}
+	public void print() {
+		MyNode tempNode = head;
+		while(tempNode.next != null) {
+			System.out.println(tempNode.data);
+			tempNode = tempNode.next;
+		}
+		System.out.println(tempNode.data);
+	}
 }
